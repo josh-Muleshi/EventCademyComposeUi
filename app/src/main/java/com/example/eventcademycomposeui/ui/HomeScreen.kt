463f6256cdc9f6eca8d107570fa2d1fcffabfd00
josh muleshi <jmuleshi2@gmail.com>
2021-08-25 15:15:50 +0100

    added a HomeScreen file in which i wanna add a headSection

---
+++ app/src/main/java/com/example/eventcademycomposeui/ui/HomeScreen.kt
@@ -0,0 +1,27 @@
+package com.example.eventcademycomposeui.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen(){
+    Box(modifier = Modifier
+        .background(Color.Transparent)
+        .padding(15.dp)){
+        Column {
+
+        }
+    }
+}
+
+@Composable
+fun HeadSection(){
+
+}
+
