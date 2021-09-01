cb849defd5f8a8ff866286b4cfe8d42823a63337
josh-Muleshi <jmuleshi2@gmail.com>
2021-09-01 16:11:40 +0200

    added a navigation base

--- app/src/main/java/com/example/eventcademycomposeui/MainActivity.kt
+++ app/src/main/java/com/example/eventcademycomposeui/MainActivity.kt
@@ -3,11 +3,19 @@ package com.example.eventcademycomposeui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.eventcademycomposeui.ui.HomeScreen
+import com.example.eventcademycomposeui.ui.Screen
 import com.example.eventcademycomposeui.ui.theme.EventCademyComposeUiTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +30,25 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyBottomNavigationBar(){
     val navController = rememberNavController()
+    val items = listOf(Screen.Home, Screen.Favorite, Screen.Notification, Screen.Profile)
+    BottomNavigation {
+        val currentBasicStateEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = currentBasicStateEntry?.arguments?.getString("route")
+        items.forEach { screen ->
+            BottomNavigationItem(
+                selected = currentRoute == screen.route,
+                onClick = {
+                    navController.navigate(screen.route){
+                        popUpTo = navController.graph.startDestinationId
+                        launchSingleTop = true
+                    }
+
+                },
+                icon = { Icon(screen.icon, contentDescription = stringResource(screen.resourceId)) }
+            )
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/eventcademycomposeui/ui/Screen.kt
+++ app/src/main/java/com/example/eventcademycomposeui/ui/Screen.kt
@@ -10,8 +10,8 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import com.example.eventcademycomposeui.R
 
 sealed class Screen(val route: String, @StringRes val resourceId: Int, val icon: ImageVector){
-    class Home: Screen("Profile", R.string.home, Icons.Outlined.Home)
-    class Favorite: Screen("Profile", R.string.favorite, Icons.Outlined.Favorite)
-    class Notification: Screen("Profile", R.string.notification, Icons.Outlined.Notifications)
-    class Profile: Screen("Profile", R.string.profile, Icons.Outlined.Person)
+    object Home: Screen("Profile", R.string.home, Icons.Outlined.Home)
+    object Favorite: Screen("Profile", R.string.favorite, Icons.Outlined.Favorite)
+    object Notification: Screen("Profile", R.string.notification, Icons.Outlined.Notifications)
+    object Profile: Screen("Profile", R.string.profile, Icons.Outlined.Person)
 }
