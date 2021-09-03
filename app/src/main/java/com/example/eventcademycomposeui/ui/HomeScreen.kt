04642d9bf0b6d75016e39c6be07c66617d8241f2
josh-Muleshi <jmuleshi2@gmail.com>
2021-09-03 22:15:21 +0200

    end of the to day's tack

--- app/src/main/java/com/example/eventcademycomposeui/ui/HomeScreen.kt
+++ app/src/main/java/com/example/eventcademycomposeui/ui/HomeScreen.kt
@@ -1,24 +1,20 @@
 package com.example.eventcademycomposeui.ui
 
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Divider
-import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import com.example.eventcademycomposeui.R
-import com.example.eventcademycomposeui.ui.theme.*
+
 
 @Composable
 fun HomeScreen(){
@@ -27,18 +23,21 @@ fun HomeScreen(){
             .background(Color.Transparent)
             .fillMaxSize()
     ){
-        MyRecyclerView()
+        MyRecyclerView{
+            //Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+        }
     }
 }
 
 @Composable
-fun MyRecyclerView(){
+fun MyRecyclerView(selectedItem : (String) -> (Unit)){
     LazyColumn{
         items(100){
             Text(
                 text = "User name $it",
                 style = MaterialTheme.typography.h3,
                 modifier = Modifier.padding(10.dp)
+                    .clickable { selectedItem("$it selected") }
             )
             Divider(color = Color.Black, thickness = 3.dp)
         }
